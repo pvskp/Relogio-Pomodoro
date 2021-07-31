f124ad1861242d6acf045281d4727b77c3994ec7
pvskp <pvincius14@gmail.com>
2021-07-31 00:00:11 -0300

    Documenta funções do Pomodoro

--- pomodoro-clock.py
+++ pomodoro-clock.py
@@ -17,6 +17,8 @@ class Pomodoro:
         self.root.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
         self.root.title('Pomodoro')
 
+        ## signals
+
         self.is_running = False
         self.pause_signal = False
 
@@ -42,32 +44,57 @@ class Pomodoro:
         self.go_button.place(y=90, relx=.5, rely=.5, anchor=CENTER)
         self.go_button.bind('<Button-1>', self.run_work_time)
 
-        # Threding 
+        ## Threading 
 
         self.check = threading.Thread(target=self.running_check)
         self.check.start()
 
     def set_time_window(self, event):
+        """
+        Create a set-time window, where the user can input two integers: the time to rest
+        and the time to work.
+
+        Receive: the click button event.
+        Return: nothing.
+        """
+        
+        ## Set time window configuration
+        
         self.time_window = Toplevel()
         self.time_window.title('Time')
         self.time_window.minsize(width=TIME_WINDOW_WIDTH, height=TIME_WINDOW_HEIGHT)
         self.time_window.maxsize(width=TIME_WINDOW_WIDTH, height=TIME_WINDOW_HEIGHT)
+
+        ## time to work entry
 
         self.work_time_label = ttk.Label(self.time_window, text='Minutes you want to spend working:')
         self.work_time_label.place(x=20, y=30)
         self.work_time_entry = ttk.Entry(self.time_window, width=5)
         self.work_time_entry.place(x=265, y=30)
 
+        ## time to rest entry
+
         self.rest_time_label = ttk.Label(self.time_window, text='Minutes you want to spend resting: ')
         self.rest_time_label.place(x=20, y=90)
         self.rest_time_entry = ttk.Entry(self.time_window, width=5)
         self.rest_time_entry.place(x=265, y=90)
+
+        ## Apply button
 
         self.apply_button = ttk.Button(self.time_window, text='Apply', width=5)
         self.apply_button.place(x=175,y=150)
         self.apply_button.bind('<Button-1>', self.save_time)
 
     def save_time(self, event):
+        """
+        Saves the time inputed by the user on the Set Time window.
+
+        Receive: the click button event.
+        Return: nothing.
+        """
+
+        ## Get the work time and the rest time
+
         work_time = self.work_time_entry.get()
         rest_time = self.rest_time_entry.get()
         
@@ -84,16 +111,23 @@ class Pomodoro:
             self.times = [int(work_time)*60, int(rest_time)*60] # save the time in seconds
             self.times_backup = (int(work_time)*60, int(rest_time)*60) # store the intial values to restart the time
             self.work_time = self.seconds_to_string(self.times[0])
-            self.display.config(text=self.work_time)
-            self.time_window.destroy()
+            self.display.config(text=self.work_time) # updates the display with the 'work time'
+            self.time_window.destroy() 
             self.root.update()
 
     def run_work_time(self, event):
+        """
+        Starts the work time on the Pomodoro Clock.
+
+        Receive: button click event.
+        Return: nothing.
+        """
+
         if self.times[0] >= 0:
-            self.is_running = True
+            self.is_running = True # tells the other thread that the time is running
             self.message.config(text='Time to work!')
             work_time = self.times[0]
-            self.times[0] -= 1
+            self.times[0] -= 1 # reduce 1 second on the count  
             work_time_string = self.seconds_to_string(work_time)
             self.display.config(text=work_time_string)
             if not self.pause_signal:
@@ -104,32 +138,60 @@ class Pomodoro:
         else:
             self.times[1] = self.times_backup[1]
             time.sleep(1) # time to play a sound
-            self.run_rest_time()
+            self.run_rest_time() # Start the time to rest
 
     def run_rest_time(self):
+        """
+        Starts the rest time on the Pomodoro clock.
+
+        Receive: nothing.
+        Return: nothing.
+        """
+
         if self.times[1] >= 0:
-            self.is_running = True
+            self.is_running = True # tells the other thread that the time is running
             self.message.config(text='Time to rest. Take a break!')
             rest_time = self.times[1]
-            self.times[1] -= 1
+            self.times[1] -= 1 # reduce 1 second on the count  
             rest_time_string = self.seconds_to_string(rest_time)
             self.display.config(text=rest_time_string)
             self.display.after(1000, self.run_rest_time)
         else:
             self.times[0] = self.times_backup[0]
             time.sleep(1) # time to play a sound
-            self.run_work_time(self.go_button_event)
+            self.run_work_time(self.go_button_event) # starts, again, the time to work
 
     def pause_time(self, event):
+        """
+        Send a signal to pause the timer.
+
+        Receive: button click event.
+        Return: nothing.
+        """
+
         self.pause_signal = True
 
     def seconds_to_string(self, seconds):
+        """
+        Converts seconds (integer) into the MM:SS format.
+
+        Receive: Integers that represent the seconds.
+        Return: a string that represents the time in the MM:SS format.
+        """
+
         minute, second = divmod(seconds, 60)
         time_string = f'{minute:02d}:{second:02d}'
 
         return time_string
 
     def running_check(self):
+        """
+        Checks if the time is running. If it is, changes the "Go!" button to "Pause" button.
+
+        Receive: nothing.
+        Return: nothing.
+        """
+
         while True:
             if self.is_running:
                 self.go_button.config(text='Pause', width=5)
@@ -138,7 +200,7 @@ class Pomodoro:
                 self.go_button.config(text='Go!', width=3)
                 self.go_button.bind('<Button-1>', self.run_work_time)
                 self.pause_signal = False
-            time.sleep(0.1)
+            time.sleep(0.1) 
 
 def main():
     root = Tk()
